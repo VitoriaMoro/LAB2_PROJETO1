9b6647f71ac8d2b20419ddf64f8f8286526eaacc
VitoriaMoro <morovitoria1@gmail.com>
2025-08-13 16:29:54 +0000

    4

--- tarefa1.py
+++ tarefa1.py
@@ -1,6 +1,15 @@
+import random
+
 def cumprimento(texto):
     return f"Olá, {texto}"
 
 nome = "Vitória Moro"
 
 print(cumprimento(nome))
+
+def sorteio():
+    numeros = [random.randint(1,100) for _ in range(7)]
+    media = sum(numeros) / len(numeros)
+    return f"Os números sorteados são {numeros} e a sua média é {media:.2f}"
+
+print(sorteio())
